873c32366ff571b6042c68f039f7faa4f68336a2
EkaBachrudin <vvalentine825@gmail.com>
2022-10-10 17:18:53 +0700

    Fix sidebar active menu

--- resources/views/usr/layouts/navigation.blade.php
+++ resources/views/usr/layouts/navigation.blade.php
@@ -83,31 +83,31 @@
         <!-- Side Navigation -->
         <div class="content-side content-side-full">
           <ul class="nav-main space-y-3">
-            <li class="nav-main-item {{ (request()->is('')) ? 'active' : '' }}">
-              <a class="nav-main-link " href="/">
+            <li class="nav-main-item">
+              <a class="nav-main-link {{ (request()->is('')) ? 'active' : '' }}" href="/">
                 <i class="nav-main-link-icon fa fa-house-user"></i>
                 <span class="nav-main-link-name">Dashboard</span>
               </a>
             </li>
-            <li class="nav-main-item {{ (request()->is('maa/regulation*')) ? 'active' : '' }}">
-              <a class="nav-main-link " href="/maa/regulation">
+            <li class="nav-main-item ">
+              <a class="nav-main-link {{ (request()->is('maa/regulation*')) ? 'active' : '' }}" href="/maa/regulation">
                 <i class="nav-main-link-icon fa fa-handshake"></i>
                 <span class="nav-main-link-name">Company Regulation</span>
               </a>
             </li>
-            <li class="nav-main-item {{ (request()->is('maa/sop*')) ? 'active' : '' }}">
-              <a class="nav-main-link " href="/maa/sop">
+            <li class="nav-main-item ">
+              <a class="nav-main-link {{ (request()->is('maa/sop*')) ? 'active' : '' }}" href="/maa/sop">
                 <i class="nav-main-link-icon fa fa-eye"></i>
                 <span class="nav-main-link-name">Sop & Form</span>
               </a>
             </li>
-            <li class="nav-main-item {{ (request()->is('maa/news*')) ? 'active' : '' }}">
+            <li class="nav-main-item ">
               <a class="nav-main-link " href="/maa/news">
                 <i class="nav-main-link-icon fa fa-newspaper"></i>
                 <span class="nav-main-link-name">Internal News</span>
               </a>
             </li>
-            <li class="nav-main-item">
+            <li class="nav-main-item {{ (request()->is('maa/news*')) ? 'active' : '' }}">
               <a class="nav-main-link " href="#">
                 <i class="nav-main-link-icon fa fa-graduation-cap"></i>
                 <span class="nav-main-link-name">E Learning</span>
